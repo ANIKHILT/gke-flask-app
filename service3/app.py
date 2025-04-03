9770a60c6f5fd41e4070d0f889e4c2140e763819
Nikhil Ambade (anikhilt) <74777631+ANIKHILT@users.noreply.github.com>
2025-04-03 17:14:13 +0530

    Update app.py

--- service3/app.py
+++ service3/app.py
@@ -1,23 +1,11 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-@app.route('/service1')
-def service1():
-    return render_template('service1.html')
-
-@app.route('/service2')
-def service2():
-    return redirect("http://<service2-loadbalancer-ip>")
-
-@app.route('/service3')
-def service3():
-    return redirect("http://<service3-loadbalancer-ip>")
+    return render_template('service3.html')
 
 if __name__ == '__main__':
     app.run(port=int(os.environ.get("PORT", 8080)), host='0.0.0.0', debug=True)
